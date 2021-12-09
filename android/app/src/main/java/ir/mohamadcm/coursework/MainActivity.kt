107ee7c113b28032c1b1672db7ced7bf0bb55872
Amirhossein-Hediehloo <amirhosseinhediehloo@gmail.com>
2021-12-09 16:50:34 +0330

    Add Login logic

--- android/app/src/main/java/ir/mohamadcm/coursework/MainActivity.kt
+++ android/app/src/main/java/ir/mohamadcm/coursework/MainActivity.kt
@@ -1,13 +1,18 @@
 package ir.mohamadcm.coursework
 
-import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
 import ir.mohamadcm.coursework.databinding.ActivityMainBinding
+import android.util.Patterns
 
-
+import java.util.regex.Pattern
+import java.util.regex.Pattern.matches
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,19 +22,54 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        val queue = VolleySingleton.getInstance(this.applicationContext).requestQueue
         // Inflate the layout XML file and return a binding object instance
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         // Set the content view of the Activity to be the root view of the layout
         setContentView(binding.root)
 
+        binding.progress.visibility = View.GONE
         // Setup a click listener on the calculate button to calculate the tip
         binding.buttonLogin.setOnClickListener(View.OnClickListener { view ->
-            Toast.makeText(applicationContext, "Login was Successful!", Toast.LENGTH_LONG).show()
-            val myIntent = Intent(this@MainActivity, ScannedBarcodeActivity::class.java)
-            myIntent.putExtra("user-name", 1) //Optional parameters
-            this@MainActivity.startActivity(myIntent)
+            val emailText = binding.editEmail.editText?.text
+            val passwordText = binding.editPassword.editText?.text
+
+            binding.editEmail.editText?.error = null
+            binding.editPassword.editText?.error = null
+
+            val emailPattern = Patterns.EMAIL_ADDRESS
+            val passPattern = Pattern.compile("/^.{6,}\$/")
+
+            var isValid = true
+            if (!emailPattern.matcher(emailText).matches()) {
+                binding.editEmail.editText?.error = "Invalid Email"
+                isValid = false
+            }
+            if (passwordText!!.length < 6) {
+                binding.editPassword.editText?.error = "Invalid Password (6 Char At Least)"
+                isValid = false
+            }
+            if (isValid) {
+                binding.progress.visibility = View.VISIBLE
+                val stringRequest = StringRequest(
+                    Request.Method.GET, "https://nightly.smartfund.iknito.com/api/v1/md/user?username=${emailText}&password=${passwordText}",
+                    Response.Listener<String> { response ->
+                        binding.progress.visibility = View.GONE
+                        Log.d("res", response)
+                        // Display the first 500 characters of the response string.
+                        Toast.makeText(applicationContext, "Response is: ${response.toString()}", Toast.LENGTH_LONG).show()
+                    },
+                    Response.ErrorListener { Toast.makeText(applicationContext, "Failed badly", Toast.LENGTH_LONG).show() })
+
+                VolleySingleton.getInstance(this).addToRequestQueue(stringRequest)
+            } else {
+            }
+
+//            Toast.makeText(applicationContext, "Login was Successful!", Toast.LENGTH_LONG).show()
+//            val myIntent = Intent(this@MainActivity, ScannedBarcodeActivity::class.java)
+//            myIntent.putExtra("user-name", 1) //Optional parameters
+//            this@MainActivity.startActivity(myIntent)
         })
 
     }
--- android/app/src/main/java/ir/mohamadcm/coursework/VolleySingleton.kt
+++ android/app/src/main/java/ir/mohamadcm/coursework/VolleySingleton.kt
@@ -8,13 +8,13 @@ import com.android.volley.RequestQueue
 import com.android.volley.toolbox.Volley
 import com.android.volley.toolbox.ImageLoader
 
-class VolleyHelpers constructor(context: Context) {
+class VolleySingleton constructor(context: Context) {
     companion object {
         @Volatile
-        private var INSTANCE: VolleyHelpers? = null
+        private var INSTANCE: VolleySingleton? = null
         fun getInstance(context: Context) =
             INSTANCE ?: synchronized(this) {
-                INSTANCE ?: VolleyHelpers(context).also {
+                INSTANCE ?: VolleySingleton(context).also {
                     INSTANCE = it
                 }
             }
@@ -32,11 +32,11 @@ class VolleyHelpers constructor(context: Context) {
             })
     }
     val requestQueue: RequestQueue by lazy {
-        // applicationContext is key, it keeps you from leaking the
-        // Activity or BroadcastReceiver if someone passes one in.
         Volley.newRequestQueue(context.applicationContext)
     }
     fun <T> addToRequestQueue(req: Request<T>) {
         requestQueue.add(req)
     }
+
+
 }
