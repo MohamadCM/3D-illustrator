15a09c8670c10edc15b595f8feff2b374d08b888
Amirhossein-Hediehloo <amirhosseinhediehloo@gmail.com>
2021-12-09 14:34:03 +0330

    add scan now activity

---
+++ android/app/src/main/java/ir/mohamadcm/coursework/ScanLauncherActivity.kt
@@ -0,0 +1,11 @@
+package ir.mohamadcm.coursework
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScanLauncherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan_launcher)
+    }
+}
