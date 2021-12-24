d639de04af60293a4008c35408bbb8ce4adc6953
Mohamad ChamanMotlagh <m.chamanmotlagh@gmail.com>
2021-12-24 22:43:35 +0330

    Add recent QR slider

--- android/app/src/main/java/ir/mohamadcm/coursework/MoreOptionsActivity.kt
+++ android/app/src/main/java/ir/mohamadcm/coursework/MoreOptionsActivity.kt
@@ -4,6 +4,7 @@ import android.R
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import com.smarteist.autoimageslider.SliderView
 import ir.mohamadcm.coursework.databinding.ActivityMoreOptionsBinding
 
 
@@ -17,6 +18,13 @@ class MoreOptionsActivity : AppCompatActivity() {
 
         manageTheme()
 
+        val imageSlider = binding.imageSlider
+        val imageList: ArrayList<String> = ArrayList()
+        imageList.add("https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg")
+        imageList.add("https://images.ctfassets.net/hrltx12pl8hq/4plHDVeTkWuFMihxQnzBSb/aea2f06d675c3d710d095306e377382f/shutterstock_554314555_copy.jpg")
+        imageList.add("https://media.istockphoto.com/photos/child-hands-formig-heart-shape-picture-id951945718?k=6&m=951945718&s=612x612&w=0&h=ih-N7RytxrTfhDyvyTQCA5q5xKoJToKSYgdsJ_mHrv0=")
+        setImageInSlider(imageList, imageSlider)
+
     }
 
     /* Switch between dark and light mode */
@@ -28,5 +36,13 @@ class MoreOptionsActivity : AppCompatActivity() {
             if (isChecked) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
             else AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         }
+    }
+
+    private fun setImageInSlider(images: ArrayList<String>, imageSlider: SliderView) {
+        val adapter = MySliderImageAdapter()
+        adapter.renewItems(images)
+        imageSlider.setSliderAdapter(adapter)
+        imageSlider.isAutoCycle = true
+        imageSlider.startAutoCycle()
     }
 }
